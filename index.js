0a8c5119856186420b39c00227ec8cf44dd9fe3e
Armun4 <82039854+Armun4@users.noreply.github.com>
2021-08-23 10:00:36 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const app = express();
+const logger = require("./loggerMiddleware");
+var pokemons = require("./pokemons");
+const cors = require("cors");
+app.use(express.json());
+app.use(logger);
+app.use(cors());
+
+app.get("/api/pokemons", (request, response) => {
+  response.json(pokemons);
+});
+app.get("/api/pokemons/:id", (request, response) => {
+  const id = parseInt(request.params.id);
+  const pokemon = pokemons.find((pokemon) => pokemon.id === id);
+
+  if (pokemon) {
+    response.json(pokemon);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete("/api/pokemons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  pokemons = pokemons.filter((pokemon) => pokemon.id !== id);
+  response.status(204).end();
+  response.send(`${pokemon.name} deleted successfully`);
+});
+
+app.put("/api/pokemons/:id", (request, response) => {
+  const data = request.body;
+  const id = parseInt(request.params.id);
+  const pokemon = pokemons.find((pokemon) => pokemon.id === id);
+  if (!pokemon || !pokemon.name) {
+    return response.status(400).json({
+      error: "pokemon not found",
+    });
+    const pokemon = {
+      id: pokemon.id,
+      name: data.name,
+      sprite: data.sprite,
+      catched: typeof data.catched !== "undefined" ? data.catched : false,
+    };
+
+    response.status(200).json(Pokemon);
+  }
+});
+
+app.post("/api/pokemons", (request, response) => {
+  const pokemon = request.body;
+  if (!pokemon || !pokemon.name) {
+    return response.status(400).json({
+      error: "pokemon not found",
+    });
+  }
+
+  const ids = pokemons.map((pokemon) => pokemon.id);
+  const maxId = Math.max(...ids);
+  const newPokemon = {
+    id: maxId + 1,
+    name: pokemon.name,
+    sprite: pokemon.sprite,
+    catched: typeof pokemon.catched !== "undefined" ? pokemon.catched : false,
+  };
+
+  pokemons = [...pokemons, newPokemon];
+  response.status(201).json(newPokemon);
+});
+
+app.use((request, response) => {
+  response.status(404).json({ error: "Not Found" });
+});
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running at ${PORT}`);
+});
---
+++ pokemons.js
@@ -0,0 +1,55 @@
+var pokemons = [
+    {
+      "id": 1,
+      "name": "Hola",
+      "sprite": "https://github.com/Armun4/pokedex/blob/main/src/assets/red-blue/1.png?raw=true",
+      "catched": true
+    },
+    {
+      "id": 3,
+      "name": "Venusaur",
+      "sprite": "https://github.com/Armun4/pokedex/blob/main/src/assets/red-blue/3.png?raw=true",
+      "catched": true
+    },
+    {
+      "id": 4,
+      "name": "Charmander",
+      "sprite": "https://github.com/Armun4/pokedex/blob/main/src/assets/red-blue/4.png?raw=true",
+      "cactched": false
+    },
+    {
+      "id": 5,
+      "name": "Bulbasaur",
+      "sprite": "https://github.com/Armun4/pokedex/blob/main/src/assets/red-blue/5.png?raw=true",
+      "catched": true
+    },
+    {
+      "id": 6,
+      "name": "Charmander",
+      "sprite": "https://github.com/Armun4/pokedex/blob/main/src/assets/red-blue/6.png?raw=true",
+      "cactched": false
+    },
+    {
+      "id": 7,
+      "name": "Squirtle",
+      "sprite": "https://github.com/Armun4/pokedex/blob/main/src/assets/red-blue/7.png?raw=true"
+    },
+    {
+      "id": 8,
+      "name": "Wartortle mi panita",
+      "sprite": "https://github.com/Armun4/pokedex/blob/main/src/assets/red-blue/8.png?raw=true"
+    },
+    {
+      "id": 9,
+      "name": "Blastoise",
+      "sprite": "https://github.com/Armun4/pokedex/blob/main/src/assets/red-blue/9.png?raw=true",
+      "cactched": false
+    },
+    {
+      "name": "Armando",
+      "sprite": "https://github.com/Armun4/pokedex/blob/main/src/assets/red-blue/108.png?raw=true",
+      "id": 10
+    }
+  ]
+
+  module.exports = pokemons
